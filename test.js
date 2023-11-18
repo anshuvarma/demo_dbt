a21b1c275c1234b5f82016d1068783046e32a786
anshuvarma <anshu.v1011@gmail.com>
2023-11-18 13:37:15 +0530

    Data type conversion

--- test.js
+++ test.js
@@ -3,3 +3,15 @@
 // "33abc" => NaN(Datatype will be Number but value will be nan)
 
 // Empty string to boolean conversion will be False else it will be True
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// In equality check == and comparison >= and <= works differently. Comparison converts null to a number, treating it as 0. Thats why null >= 0 is true and null == 0 is false 
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// Strict check === It checks value as well as their datatypes 
